9bd4a4f1aa63b12b908acff404af8b67c94e823f
Jader Oliveira <jaderstz16@gmail.com>
2022-09-07 22:59:36 -0300

    ..

--- src/components/Form/ResultIMC/index.js
+++ src/components/Form/ResultIMC/index.js
@@ -1,11 +1,12 @@
 import React from "react";
 import { View, Text } from "react-native";
+import styles from "./style";
 
 export default function ResultIMC(props) {
   return (
-    <View>
-      <Text>{props.messageResultIMC}</Text>
-      <Text>{props.resultIMC}</Text>
+    <View style={styles.resultIMC}>
+      <Text style={styles.information}>{props.messageResultIMC}</Text>
+      <Text style={styles.imc}>{props.resultIMC}</Text>
     </View>
   );
 }
--- src/components/Form/ResultIMC/style.js
+++ src/components/Form/ResultIMC/style.js
@@ -1,4 +1,23 @@
 import { StyleSheet } from "react-native";
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  resultIMC: {
+    flex: 1,
+    marginTop: 15,
+    paddingTop: 60,
+    borderRadius: 50,
+    alignItems: "center",
+    width: "100%",
+  },
+  imc: {
+    fontSize: 48,
+    color: "#ff0043",
+    fontWeight: "bold",
+  },
+  information: {
+    fontSize: 28,
+    color: "#ff0043",
+    fontWeight: "bold",
+  },
+});
 export default styles;
